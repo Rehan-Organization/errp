e9c4a46da596ec5e138c036acf2921c6fd74b349
Aishwarya Ware <aishwaryaware2567@gmail.com>
2023-03-01 13:45:23 +0530

    Change in Navigation of CreateAwardComponent

--- UI/src/app/home/home-routing.module.ts
+++ UI/src/app/home/home-routing.module.ts
@@ -10,11 +10,7 @@ const routes: Routes = [
       {
         path:'awardTypes',
         loadChildren: () => import('./../admin/view-award/view-award.module').then(m => m.ViewAwardModule)
-      },
-      {
-        path:'create',
-        loadChildren: () => import('./../admin/create-award/create-award.module').then(m => m.CreateAwardModule)
-      },
+      }
      
     ]
   }
