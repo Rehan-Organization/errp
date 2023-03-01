4be1d7d121c1bdfba0a018a42af145725d413681
pushkarjoshi19152 <73287201+pushkarjoshi19152@users.noreply.github.com>
2023-03-01 12:43:00 +0530

    Entity Objects Added (#1)
    * Entity Objects Added
    
    * Removed CrossOrigin from LoginController
    
    * Deleted bin folder
    
    * revert application.properties
    
    * Remove unused import
    
    ---------
    
    Co-authored-by: Adarsh <adarshashok1612@gmail.com>

---
+++ Server/src/main/java/com/abs/errp/entity/EmployeeAwards.java
@@ -0,0 +1,47 @@
+package com.abs.errp.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="EMPLOYEE_AWARDS")
+public class EmployeeAwards {
+	
+	@Id
+	@Column(name="EMPLOYEE_AWARD_ID")
+	private long employeeAwardId;
+	
+	@Column(name="AWARD_ID")
+	private long awardId;
+	
+	@Column(name="AWARDEE_ID")
+	private long awardeeId;
+	
+	@Column(name="AWARD_RAISER_ID")
+	private long awardRaiserId;
+	
+	@Column(name="ACHIEVEMENT_ID")
+	private long achievementId;
+	
+	@Column(name="REQUEST_ID")
+	private long requestId;
+	
+	@Column(name="APPROVED_BY_ID")
+	private long approvedById;
+	
+	@Column(name="APPROVED_DATE")
+	private Date approvedDate;
+	
+	@Column(name="REMARKS")
+	private String remarks;
+	
+	@Column(name="SUPERVISOR_COMMENTS")
+	private String supervisorComments;
+	
+
+}
---
+++ Server/src/main/java/com/abs/errp/entity/Feedback.java
@@ -0,0 +1,38 @@
+package com.abs.errp.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="FEEDBACK")
+public class Feedback {
+	
+	@Id
+	@Column(name="FEEDBACK_ID")
+	private long id;
+	
+	@Column(name="SENDER_ID")
+	private long senderId;
+	
+	@Column(name="RECEIVER_ID")
+	private long receiverId;
+	
+	@Column(name ="TITLE")
+	private String title;
+	
+	@Column(name="DESCRIPTION")
+	private String description;
+	
+	@Column(name="CREATED_DATE")
+	private Date createdDate;
+	
+	@Column(name="LAST_UPDATED_DATE")
+	private Date lastUpdatedDate;
+	
+	
+
+}
