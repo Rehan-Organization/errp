8e1ed2f047f325a99b96b295360f88ca8e8fd133
Atharv Pandit <atharvpandit810@gmail.com>
2023-03-01 19:06:46 +0530

    Make API for get and POSt

--- Server/src/main/java/com/abs/errp/achievement/AchievementService.java
+++ Server/src/main/java/com/abs/errp/achievement/AchievementService.java
@@ -2,9 +2,11 @@ package com.abs.errp.achievement;
 
 import java.util.List;
 
+
 import com.abs.errp.entity.Achievements;
 
 public interface AchievementService {
 	
 	List<Achievements> getAllAchievements();
+	Achievements saveAchievement(Achievements achievement);
 }
--- Server/src/main/java/com/abs/errp/achievement/AchievementServiceImpl.java
+++ Server/src/main/java/com/abs/errp/achievement/AchievementServiceImpl.java
@@ -2,20 +2,32 @@ package com.abs.errp.achievement;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.abs.errp.entity.Achievements;
 
+@Service
 public class AchievementServiceImpl implements AchievementService {
 	
 	
-	@Autowired
+	
 	private AchievementRepository achievementRepository;
 	
+	public AchievementServiceImpl(AchievementRepository achievementRepository) {
+		super();
+		this.achievementRepository = achievementRepository;
+	}
+
 	@Override
 	public List<Achievements> getAllAchievements() {
 		// TODO Auto-generated method stub
 		return achievementRepository.findAll();
+	}
+
+	@Override
+	public Achievements saveAchievement(Achievements achievement) {
+		// TODO Auto-generated method stub
+		return achievementRepository.save(achievement);
 	}
 	
 	
