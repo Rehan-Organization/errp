4468cd93670a3a7a0e09c49bb33d26c34183b9a1
Atharv Pandit <atharvpandit810@gmail.com>
2023-03-02 15:35:41 +0530

    added service class and interface
    Here I added service class write observable for get API and display data on cards from database

---
+++ UI/src/app/Achievement/achievement.ts
@@ -0,0 +1,12 @@
+export interface Achievement {
+
+    
+        achievement_id: number;
+        title: string;
+        description: string;
+        created_date:Date;
+        achievement_status:number;
+        comments:string;
+        last_modified:Date
+
+}
