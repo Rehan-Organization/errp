07fad041ce0d65d7d28c9282ac91e55541aed618
pratikshagund <pratikshagund13@gmail.com>
2023-03-06 18:40:54 +0530

    Implemented view feedback functionality

--- UI/src/app/feedback/view-feedback/view-feedback.component.ts
+++ UI/src/app/feedback/view-feedback/view-feedback.component.ts
@@ -14,55 +14,25 @@ export class ViewFeedbackComponent implements OnInit {
     private updateSubscription?: Subscription;
     constructor(private feedbackService: FeedbackService, private router: Router) {}
     employees: Employee[] = [];
-    map = new Map();
-    title: string = 'pagination';
-    page: number = 1;
-    count: number = 0;
-    tableSize: number = 5;
-    tableSizes: any = [5, 10, 15, 20];
     feedbacks: Feedback[] = [];
-    p: any;
     options: string[] = ['My Feedbacks', 'Feedbacks Given By Me'];
     choosenOption: string = 'My Feedbacks';
-    flag?: number;
-    enteredName: string = '';
+    isMyFeedbacks?: number;
+    searchEmployee: string = '';
     ngOnInit() {
-        // this.updateSubscription = interval(3000).subscribe((val) => { this.fetchFeedbacks()});
         this.fetchFeedbacks();
-        console.log(this.flag);
-        this.feedbackService.getReportees().subscribe((employee) => (this.employees = employee));
-        for (let employee of this.employees) {
-            this.map.set(employee.employeeId, employee.employeeName);
-        }
-        this.map.set(1, 'Shankar Waghmode');
-        this.map.set(2, 'Pushkar Joshi');
-        this.map.set(3, 'Shrikant Hadpad');
-        this.map.set(4, 'Avdhut Patil');
-        this.map.set(5, 'Pratiksha Gund');
-        this.map.set(6, 'Om Jadhao');
     }
     fetchFeedbacks() {
         if (this.choosenOption == 'My Feedbacks') {
-            this.flag = 0;
+            this.isMyFeedbacks = 0;
             this.feedbackService
                 .fetchAllFeedbacks(1)
                 .subscribe((feedback) => (this.feedbacks = feedback));
         } else {
-            this.flag = 1;
+            this.isMyFeedbacks = 1;
             this.feedbackService
                 .fetchAllFeedbacks(2)
                 .subscribe((feedback) => (this.feedbacks = feedback));
-        }
-    }
-    cleanFeedback() {
-        for (var i = 0; i < this.feedbacks.length; i++) {
-            if (!this.map.has(this.feedbacks[i].senderId)) {
-                this.feedbackService
-                    .fetchEmployee(this.feedbacks[i].senderId)
-                    .subscribe((Employee) =>
-                        this.map.set(Employee.employeeId, Employee.employeeName)
-                    );
-            }
         }
     }
     addFeedback() {
@@ -70,12 +40,10 @@ export class ViewFeedbackComponent implements OnInit {
     }
     deleteFeedback(feedback: Feedback) {
         if (confirm('Are you sure you want to delete ? ')) {
-            // this.feedbacks = this.feedbacks.filter(newFeedback => newFeedback.id != feedback.id);
             this.feedbackService.removeFeedback(feedback.id).subscribe((feedback) => {
                 if (feedback == null) alert('Failed to delete');
                 else {
                     alert('Successfully deleted feedback');
-                    // this.fetchFeedbacks();
                     this.feedbacks = this.feedbacks.filter(
                         (newFeedback) => newFeedback.id != feedback.id
                     );
@@ -85,7 +53,6 @@ export class ViewFeedbackComponent implements OnInit {
     }
     updateFeedback(feedback: Feedback) {
         alert(feedback.id);
-        // this.router.navigate["home/updatefeedback"];
     }
 }
 
