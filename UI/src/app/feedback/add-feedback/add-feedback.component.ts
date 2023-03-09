b2b8b0f32ff5854aeb892fdb3f5a720eadb2e021
Avadhut Patil <avadhutpatil17340@gmail.com>
2023-03-09 10:40:24 +0530

    component class for add feedback

--- UI/src/app/feedback/add-feedback/add-feedback.component.ts
+++ UI/src/app/feedback/add-feedback/add-feedback.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Employee } from '../feedback-model.ts/employee';
 import { Feedback } from '../feedback-model.ts/feedback';
 import { FeedbackService } from '../feedback.service';
+
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FormControl } from '@angular/forms';
 import { AlertController, ToastController } from '@ionic/angular';
@@ -13,12 +14,6 @@ import { AlertController, ToastController } from '@ionic/angular';
     styleUrls: ['./add-feedback.component.scss'],
 })
 export class AddFeedbackComponent implements OnInit {
-    // feedbackForm = new FormGroup({
-    //     title: new FormControl('', Validators.required),
-    //     description: new FormControl('', [Validators.required, Validators.required]),
-    // });
-
-
     constructor(
         private alertController: AlertController,
         private toast: ToastController,
@@ -34,9 +29,10 @@ export class AddFeedbackComponent implements OnInit {
     employees: Employee[] = [];
     selectedValue: number = 5;
 
-    feedbackId?:number;
-    urlId?:any;
-    fetchedFeedback:Feedback[]=[];
+    feedbackId?: number;
+    urlId?: any;
+    fetchedFeedback: Feedback[] = [];
+
     ngOnInit() {
         this.urlId = this.route.snapshot.paramMap.get('id');
         console.log('url id' + this.urlId);
@@ -52,26 +48,43 @@ export class AddFeedbackComponent implements OnInit {
         this.fetchReportees();
     }
 
-    addFeedback(feedback: Feedback) {
+    async addFeedback(feedback: Feedback) {
         if (this.feedback.receiverId.employeeName?.trim()) {
             this.showAlert('Employee name cannot be empty!');
         } else if (!this.feedback.title?.trim()) {
             this.showAlert('Title cannot be empty!');
         } else if (!this.feedback.description?.trim()) {
             this.showAlert('Description cannot be empty!');
-        }else{
-            alert(this.employees.length);
-            feedback.receiverId.employeeId = this.selectedValue;
-            this.feedbackService.saveFeedback(feedback).subscribe((feedbackResponse) => {
-            console.log(feedbackResponse);
+        } else {
+            const alert = await this.alertController.create({
+                header: 'Confirm feedback ',
+                buttons: [
+                    {
+                        text: 'Cancel',
+                        role: 'cancel',
+                        handler: () => {
+                            //handle canncel toast
+                        },
+                    },
+                    {
+                        text: 'Submit',
+                        role: 'confirm',
+                        handler: () => {
+                            feedback.receiverId.employeeId = this.selectedValue;
+                            this.feedbackService
+                                .saveFeedback(feedback)
+                                .subscribe((feedbackResponse) => {
+                                    console.log(feedbackResponse);
 
-            this.feedback.title = '';
-            this.feedback.description = '';
-
-        });
+                                    this.feedback.title = '';
+                                    this.feedback.description = '';
+                                });
+                        },
+                    },
+                ],
+            });
+            await alert.present();
         }
-
-        
     }
     fetchReportees() {
         this.feedbackService.getReportees().subscribe((reportee) => (this.employees = reportee));
@@ -87,10 +100,15 @@ export class AddFeedbackComponent implements OnInit {
         this.feedback.description = '';
     }
 
-    showAlert(message: string) { this.alertController
-         .create({header: 'Alert', 
-         message: message,
-         buttons: [ 
-            {text: 'Ok',},
-         ], }) .then((res) => { res.present(); });}
+    showAlert(message: string) {
+        this.alertController
+            .create({
+                header: 'Alert',
+                message: message,
+                buttons: [{ text: 'Ok' }],
+            })
+            .then((res) => {
+                res.present();
+            });
+    }
 }
