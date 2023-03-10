ca9348fb176a2a73e4aadb513e6a621de83a4749
Adarsh <adarshashok1612@gmail.com>
2023-03-10 19:26:00 +0530

    hasRole on Leaderboard controller

--- Server/src/main/java/com/abs/errp/leaderboard/LeaderboardController.java
+++ Server/src/main/java/com/abs/errp/leaderboard/LeaderboardController.java
@@ -4,12 +4,14 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@PreAuthorize("hasRole('ADMIN')")
 @RequestMapping("/leaderboard")
 public class LeaderboardController {
 	
