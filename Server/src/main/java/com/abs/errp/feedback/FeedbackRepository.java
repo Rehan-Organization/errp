7ef0d445f077c74597a4776a8760eef9e0efdac0
pratikshagund <pratikshagund13@gmail.com>
2023-03-10 18:54:32 +0530

    Provided comments and formatting

--- Server/src/main/java/com/abs/errp/feedback/FeedbackRepository.java
+++ Server/src/main/java/com/abs/errp/feedback/FeedbackRepository.java
@@ -2,21 +2,21 @@ package com.abs.errp.feedback;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
-
 import com.abs.errp.entity.Feedback;
 import com.abs.errp.user.ErrpUser;
 
 @Repository
 public interface FeedbackRepository extends PagingAndSortingRepository<Feedback, Integer> {
+	
 	List<Feedback> findAllBySenderId(ErrpUser user, Pageable pageable);
 
 	List<Feedback> findAllByReceiverId(ErrpUser user, Pageable pageable);
 
 	Optional<Feedback> deleteById(int id);
+	
+	List<Feedback> findAllById(int id);
 
 }
