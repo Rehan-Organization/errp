6eb1adba8f6691716e8e78adcabba5d7c18a3726
Shrikant Hadpad <126184766+Shri1754@users.noreply.github.com>
2023-03-11 19:27:41 +0530

    achievement service updated

--- UI/src/app/Achievement/achievement.service.ts
+++ UI/src/app/Achievement/achievement.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { URLS } from '../constants/urls.contants';
+import { LoggedInUserContext } from '../providers/logged-in-user-context.service';
 import { Achievement } from './achievement';
 
 @Injectable({
@@ -9,36 +10,40 @@ import { Achievement } from './achievement';
 })
 export class AchievementService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient,
+    private userContext: LoggedInUserContext) { }
 
-  getAllAchievement(): Observable<Achievement[]> {
-    return this.httpClient.get<Achievement[]>("/achievement/emp");
-  }
+
 
   getPaginatedAchievement(pageNo: number, pageSize: number): Observable<Achievement[]> {
-    return this.httpClient.get<Achievement[]>('/achievement/paginated/' + pageNo + "/" + pageSize);
+    return this.httpClient.get<Achievement[]>(URLS.GET_PAGINATED + '/' + pageNo + "/" + pageSize);
   }
 
   saveAchievement(achievement: Achievement): Observable<Achievement> {
-    return this.httpClient.post<Achievement>('/achievement/add', achievement)
+    
+    achievement.employeeId = this.userContext.getLoggedInUser()?.employeeId;
+    return this.httpClient.post<Achievement>(URLS.SAVE, achievement)
   }
 
 
   updateAchievement(achievement: Achievement): Observable<Achievement> {
-    return this.httpClient.post<Achievement>('/achievement/updateAchievement/' + achievement.achievementId, achievement);
+
+    achievement.employeeId = this.userContext.getLoggedInUser()?.employeeId;
+    return this.httpClient.put<Achievement>(URLS.UPDATE, achievement);
   }
+
   getAchievement(userId: number): Observable<Achievement> {
     return this.httpClient.get<Achievement>(URLS.GET + "/" + userId);
   }
 
   deleteAchievement(achievement_id: any): Observable<Achievement> {
-    console.log(achievement_id)
-    return this.httpClient.delete(URLS.DELETE + achievement_id)
+    return this.httpClient.delete(URLS.DELETE + '/' + achievement_id)
   }
 
   submitAchievement(achievement: Achievement): Observable<Achievement> {
-    console.log("submit", achievement);
-    return this.httpClient.post<Achievement>("/achievement/submit", achievement);
+
+    achievement.employeeId = this.userContext.getLoggedInUser()?.employeeId;
+    return this.httpClient.post<Achievement>(URLS.SUBMIT, achievement);
   }
 
 
